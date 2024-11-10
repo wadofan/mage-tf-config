d00404dad0fcbea3874d82a04203482f96c081b5
wadofan <loboyko.ilya@gmail.com>
2024-11-10 02:34:45 +0200

    add null provider to run Ansible Playbook on created server

---
+++ ansible.tf
@@ -0,0 +1,14 @@
+variable "ansible_working_dir" {}
+variable "ansible_remote_user" {}
+variable "ansible_ssh_key" {}
+
+resource "null_resource" "run_ansible_playbook" {
+  triggers = {
+    trigger = hcloud_server.scific.ipv4_address
+  }
+  provisioner "local-exec" {
+    working_dir = var.ansible_working_dir
+    command = "ansible-playbook --inventory ${hcloud_server.scific.ipv4_address}, --private-key ${var.ansible_ssh_key} --user ${var.ansible_remote_user} --extra-vars 'ssh_pkg=openssh-client' playbook.yml"
+  }
+}
+
