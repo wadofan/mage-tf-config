9e1f3eab602b051808244fa144f73ea3328c1d8a
wadofan <loboyko.ilya@gmail.com>
2024-11-10 21:06:59 +0200

    fix command to run Ansible

--- ansible.tf
+++ ansible.tf
@@ -8,7 +8,7 @@ resource "null_resource" "run_ansible_playbook" {
   }
   provisioner "local-exec" {
     working_dir = var.ansible_working_dir
-    command = "ansible-playbook --inventory ${hcloud_server.scific.ipv4_address}, --private-key ${var.ansible_ssh_key} --user ${var.ansible_remote_user} --extra-vars 'ssh_pkg=openssh-client' playbook.yml"
+    command = "ansible-playbook --inventory ${hcloud_server.scific.ipv4_address}, --private-key ${var.ansible_ssh_key} --user ${var.ansible_remote_user} --extra-vars 'ssh_pkg=openssh-client cron_service=cron' playbook.yml"
   }
 }
 
